bf3d0d6fb9e89de7c0a28f1c0c2eb150c35edacd
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2022-02-06 08:19:25 +0100

    adding uid and login in set slurm of nomachine

--- nomachine/ansible/roles/nomachine/files/etc/set_slurm.sh
+++ nomachine/ansible/roles/nomachine/files/etc/set_slurm.sh
@@ -3,18 +3,32 @@
 echo "Setting nxnode process in user's slurm cgroup."
 
 ppid=`grep '^PPid:' "/proc/$$/status" | cut -f 2`
-
 echo "Nxnode pid $ppid"
 
-slurmuser=`sed -n '1p' /home/vagrant/slurm.info`
-#`head -n 1 /home/vagrant/slurm.info`
+# Retrieving the unix user name
+nomachine_login=`echo /proc/$ppid/cmdline | grep -oP "\-\-user\s+\K\w+"`
+echo "NoMachine login $nomachine_login"
 
-echo "Slurm user $slurmuser"
+#TODO:
+# A user may be opening a session with a username different from
+# the one used for allocating with slurm
 
-slurmid=`sed -n '2p' /home/vagrant/slurm.info`
-#`head -n 2 /home/vagrant/slurm.info`
+# Retrieving the slurmuser
+# slurmuser=`sed -n '1p' /home/$login/.slurm.info`
+# echo "Slurm user $slurmuser"
 
+#TODO Check the .slurm.info file exists; otherwise, this means there is no
+# slurm allocation and we should fail
+
+# Retrieving the most recent slurm job id for the user 
+slurmid=`sed -n '2p' /home/$nomachine_login/.slurm.info`
 echo "Slurm id $slurmid"
 
-echo $ppid >> /sys/fs/cgroup/cpuset/slurm/uid_1000/job_$slurmid/cgroup.procs
+uid=`id -i $nomachine_login`
+echo "User id $uid"
+
+#TODO: Check the file /uid_$uid/job_$slurmid/cgroup.procs exists
+
+echo $ppid >> /sys/fs/cgroup/cpuset/slurm/uid_$uid/job_$slurmid/cgroup.procs
+echo "PID $ppid added to the cgroup /sys/fs/cgroup/cpuset/slurm/uid_$uid/job_$slurmid/cgroup.procs"
 
